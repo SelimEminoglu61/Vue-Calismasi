5f2d7e2f7fd1fc7c348a816275f27ccba2a78f96
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2023-06-13 16:29:14 +0300

    Ders-11 Yüklemesi

---
+++ Ders11-Vue-Component/vue-component/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>deneme</h1>
+    <h3>Title Style--Global</h3>
+    <TitleLine :comment="text" />
+  </div>
+</template>
+
+<script>
+import TitleLine from "./components/TitleLine.vue"
+
+
+export default {
+  name: "App",
+  components:{
+    TitleLine
+  },
+  data(){
+    return{
+      text:"Güzelmiş",
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Ders11-Vue-Component/vue-component/src/components/TitleLine.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>{{title}}</h3>
+    <p>{{comment}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"TitleLine",
+    props:['comment'],
+    data(){
+        return{
+            title:"Vue 3'e Giriş"
+        }
+    },
+}
+</script>
+
+<!-- scoped anahtar kelimesi ile sadece componente özel css yazabiliriz -->
+
+<style scoped>
+  h3{
+    color: blue;
+    font-size: 30px;
+    font-weight: 700;
+  }
+</style>
